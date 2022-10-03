6aa48662b55f55762d9cd2d286b5030844388c84
N1ger69 <114093327+N1ger69@users.noreply.github.com>
2022-10-03 18:36:03 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,135 @@
+#include <iostream>;
+using namespace std;
+int main() {
+	//case1
+	int day;
+	cin >> day;
+	/* if (day == 1) {
+		cout << "monday" << endl;
+	}
+	else if (day == 2) {
+		cout << "tuesday" << endl;
+	}*/
+	/* switch (day) {
+	case 1: cout << "monday" << endl; break;
+	case 2: cout << "tuesday" << endl; break;
+	case 3: cout << "wednesday" << endl; break;
+	case 4: cout << "thursday" << endl; break;
+	case 5: cout << "friday" << endl; break;
+	case 6: cout << "saturday" << endl; break;
+	case 7: cout << "sunday" << endl; break;
+	default: cout << "wrong day number" << endl;
+	}*/
+	//case2
+	/*int K;
+	cin >> K;
+	switch (K) {
+	case 1: cout << "ploxo" << endl; break;
+	case 2: cout << "neyd" << endl; break;
+	case 3: cout << "yd" << endl; break;
+	case 4: cout << "xorosho" << endl; break;
+	case 5: cout << "otlichno" << endl; break;
+	default: cout << "oshibka" endl;
+	}*/
+	//case3
+	/*int month;
+	cin >> month;
+	switch (month) {
+	case 1: cout << "zima" << endl; break;
+	case 2: cout << "zima" << endl; break;
+	case 3: cout << "vesna" << endl; break;
+	case 4: cout << "vesna" << endl; break;
+	case 5: cout << "vesna" << endl; break;
+	case 6: cout << "leto" << endl; break;
+	case 7: cout << "leto" << endl; break;
+	case 8: cout << "leto" << endl; break;
+	case 9: cout << "osen" << endl; break;
+	case 10: cout << "osen" << endl; break;
+	case 11: cout << "osen" << endl; break;
+	case 12: cout << "zima" << endl; break;
+	default: cout << "nety mesyaca" << endl;
+	}*/
+	//case4
+	/*int days;
+	cin >> days;
+	switch (days) {
+	case 1:
+	case 3:
+	case 5:
+	case 7:
+	case 8:
+	case 10:
+	case 12: cout << "31 days" << endl; break;
+	case 4:
+	case 6:
+	case 9:
+	case 11: cout << "30 days" << endl; break;
+	case 2: cout << "28 days" << endl; break;
+	default: cout << "no month?" << endl; break;
+	}*/
+	/*case5
+	int N;
+	double a, b;
+	cin >> N >> a >> b;
+	switch (N) {
+	case 1: cout << "a + b" << endl; break;
+	case 2: cout << "a - b" << endl; break;
+	case 3: cout << "a * b" << endl; break;
+	case 4: cout << "a / b" << endl; break;
+	}   */
+	//ifelse1
+	/*int a;
+	cin >> a;
+	if (a == 0) {
+		cout << "true" << endl;
+	}
+	else if (a != 0) {
+		cout << "false" << endl;
+	}*/
+	//ifelse2
+	/*int A;
+	cin >> A;
+	if (A > 0) {
+		cout << "true" << endl;
+	}
+	else if (A < 0) {
+		cout << "false" << endl;
+	}
+	*/
+	//ifelse3
+	/*int a;
+	cin >> a;
+	if (a < 0) {
+		cout << "true" << endl;
+	}
+	else {
+		cout << "false" << endl;
+	}*/
+    //ifelse4
+/*int a;
+cin >> a;
+if (a >= 0) {
+	cout << "true" << endl;
+}
+else {
+	cout << "false" << endl;
+}*/
+//ifelse5
+/*int a;
+cin >> a;
+if (a <= 0) {
+	cout << "true" << endl;
+}
+else {
+	cout << "false" << endl;
+}*/
+//ifelse6
+int a;
+cin >> a;
+if (a != 0) {
+	cout << "true" << endl;
+}
+else {
+	cout << "false" << endl;
+}
+}
